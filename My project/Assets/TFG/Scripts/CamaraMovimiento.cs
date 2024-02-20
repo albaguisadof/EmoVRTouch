52c807c693e6458fbb05c1b1d0a13251a61c1fdd
albaguisadof <albaguisadof@gmail.com>
2024-02-20 10:26:28 +0100

    entornoReposo

--- My project/Assets/TFG/Scripts/CamaraMovimiento.cs
+++ My project/Assets/TFG/Scripts/CamaraMovimiento.cs
@@ -5,33 +5,43 @@ using UnityEngine.UIElements;
 
 public class CamaraMovimiento : MonoBehaviour
 {
-    public float velocidad = 5f;
+    public float velocidad = 6.5f;
     public float altura = 4f;
     private float tiempo = 0f;
 
     private void Start()
     {
-        transform.position = new Vector3(600, 22, 7);
+        transform.position = new Vector3(600, 22, 15);
     }
 
     void Update()
     {
-        //Desplazo en el terreno
-        transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
-
-        //Calculo la altura sobre el terreno
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit))
-        {
-            Vector3 posicionTerreno = hit.point;
-            transform.position = new Vector3(transform.position.x, posicionTerreno.y + altura, transform.position.z);
-        }
-
         //Calculamos el tiemnpo
         tiempo += Time.deltaTime;
-        if (tiempo >= 60f && tiempo < 61f)
+
+        if (tiempo < 60f)
         {
-           // Finalizar();
+            //Desplazo en el terreno
+            transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+
+            //Calculo la altura sobre el terreno
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                Vector3 posicionTerreno = hit.point;
+                transform.position = new Vector3(transform.position.x, posicionTerreno.y + altura, transform.position.z);
+            }
         }
+        if(tiempo >= 60f && tiempo <= 62f)
+        {
+            Finalizar();
+        }
+        
+    }
+
+    private void Finalizar()
+    {
+        Debug.Log("Terminando juego...");
+        Application.Quit();
     }
 }
